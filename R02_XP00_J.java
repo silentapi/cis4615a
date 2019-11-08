9e9d338b929d9102c8459b45f325e84e155fd12b
Joe Winkler <joe.c.winkler@outlook.com>
2019-11-08 04:22:06 -0500

    Create R02_XP00_J base class

---
+++ R02_XP00_J.java
@@ -0,0 +1,26 @@
+import java.io.File;
+
+/******************************************************************************
+ * Compilation: javac R02_XP00_J.java Execution: java R02_XP00_J
+ *
+ * Calls someFile.delete()
+ *
+ ******************************************************************************/
+
+public class R02_XP00_J {
+
+    public static void main(String[] args) {
+	
+      File someFile = new File("someFileName.txt");
+      // Do something with someFile
+      someFile.delete();
+
+    }
+
+    /*
+     * Rule 02. Expressions (EXP)
+     * Corrected code per:
+     * https://wiki.sei.cmu.edu/confluence/display/java/EXP00-J.+Do+not+ignore+values+returned+by+methods
+     *Rule 02-EXP00-J
+     */
+}
