71bf0fad5b2dcc96e7cc0d63e4f9413b678eeed0
Joe Winkler <joe.c.winkler@outlook.com>
2019-11-08 04:56:23 -0500

    Handle large unsigned values properly

---
+++ R03_NUM03_J.java
@@ -0,0 +1,37 @@
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/******************************************************************************
+ * Compilation: javac R03_NUM03_J.java Execution: java R03_NUM03_J
+ *
+ * Calls function getInteger() with both normal values and large unsigned value
+ *
+ ******************************************************************************/
+
+public class R03_NUM03_J {
+
+    public static void main(String[] args) throws IOException {
+  
+      byte[] bytes = ByteBuffer.allocate(4).putInt(1337).array();
+      DataInputStream is = new DataInputStream(new ByteArrayInputStream(bytes));
+      System.out.println("a = " + getInteger(is));
+
+      bytes = ByteBuffer.allocate(4).putInt(-512).array();
+      is = new DataInputStream(new ByteArrayInputStream(bytes));
+      System.out.println("a = " + getInteger(is));
+      
+    }
+
+    /*
+     * Rule 03. Numeric Types and Operations (NUM)
+     * Corrected code per:
+     * https://wiki.sei.cmu.edu/confluence/display/java/NUM03-J.+Use+integer+types+that+can+fully+represent+the+possible+range+of++unsigned+data     *
+     *Rule 03-NUM03
+     */
+
+    public static long getInteger(DataInputStream is) throws IOException {
+      return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+    }
+}
