076711a1caeac0d70ee7d209cf3c629408ede20f
Joe Winkler <joe.c.winkler@outlook.com>
2019-11-08 05:39:52 -0500

    Create R13_FIO01_J base class

---
+++ R13_FIO01_J.java
@@ -0,0 +1,28 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+/******************************************************************************
+ * Compilation: javac R13_FIO01_J.java Execution: java R13_FIO01_J
+ *
+ * Creates files with appropriate access permissions
+ *
+ ******************************************************************************/
+
+public class R13_FIO01_J {
+
+  public static void main(String[] args) throws IOException {
+	
+      Writer out = new FileWriter("file");
+      
+    }
+
+    /*
+     * Rule 13. Input Output (FIO)
+     * Corrected code per:
+     * https://wiki.sei.cmu.edu/confluence/display/java/FIO01-J.+Create+files+with+appropriate+access+permissions
+     *
+     *Rule 13_FIO01
+     */
+
+}
