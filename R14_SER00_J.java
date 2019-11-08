26193d87046f9aca62a68f1b14586c8a2337a66a
Joe Winkler <joe.c.winkler@outlook.com>
2019-11-08 05:44:09 -0500

    Create R14_SER00_J base class

---
+++ R14_SER00_J.java
@@ -0,0 +1,33 @@
+import java.io.Serializable;
+
+/******************************************************************************
+ * Compilation: javac R06_Met01_J.java Execution: java R06_Met01_J
+ *
+ * 
+ *
+ ******************************************************************************/
+
+public class R14_SER00_J {
+
+    public static void main(String[] args) {
+      
+
+
+    }
+
+    /*
+     * Rule 14. Serialization (SER)
+     * Corrected code per:
+     * https://wiki.sei.cmu.edu/confluence/display/java/SER00-J.+Enable+serialization+compatibility+during+class+evolution
+     *
+     *Rule 14_SER00
+     */
+
+    static class GameWeapon implements Serializable {
+      int numOfWeapons = 10;
+             
+      public String toString() {
+        return String.valueOf(numOfWeapons);
+      }
+    }
+}
